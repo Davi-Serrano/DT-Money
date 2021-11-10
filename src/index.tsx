b168cb33aa3541f6132962801b042d0691e86e27
Davi Serrano <daviscardoso2901@gmail.com>
2021-11-10 16:28:45 -0300

    Listing datas on table transactions

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,29 @@ createServer({
   models:{
     transaction: Model,
   },
-
+  seeds(server){
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Frelance de website",
+          type: "deposit",
+          category: "Dev",
+          amount: "6000",
+          createdAt: new Date("2021-03-12 09:00:00"),
+        },
+        {
+          id: 2,
+          title: "Alugel",
+          type: "withdraw",
+          category: "Casa",
+          amount: "1000",
+          createdAt: new Date("2021-03-14 11:00:00"),
+        },
+        
+      ]
+    })
+  },
 
   routes() {
     this.namespace = "api";
